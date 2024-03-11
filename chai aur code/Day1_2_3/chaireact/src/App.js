bbb90c186c0e4a78819d2ff424f475cf451171bf
092vk <vk092kumar@gmail.com>
2024-03-11 17:08:15 +0530

    changed file structure

---
+++ chai aur code/Day1_2_3/chaireact/src/App.js
@@ -0,0 +1,13 @@
+import Comp1 from "./Mycomp"
+//remember the name of react components must start with capital letters , also here 
+
+function App() {
+  return (
+    <>
+     <p>hello</p>
+     <Comp1/>
+    </>
+  );
+}
+
+export default App;
--- chai aur code/Day1_2_3/chaireact/src/index.js
+++ chai aur code/Day1_2_3/chaireact/src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> 
+    <App /> //this is a component 
   </React.StrictMode>
 );
 
 
 /*here we are rendering the app tag which is a custom componenet , this is possible due to jsx*/
+// jsx --> it stands for javaScript XML , it is markup inserted into the js 
